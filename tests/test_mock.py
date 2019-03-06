eb92b633d0235919bcbc1fb5c0638bb726d0eb37
Wonder Chang <iwonder.tw@gmail.com>
2019-03-06 18:16:15 +0800

    Compare mock assert_has_calls and mock_calls

--- tests/test_mock.py
+++ tests/test_mock.py
@@ -16,6 +16,29 @@ def test_mock_and_magic_mock():
     assert len(mock2) == 0
     assert dict(mock2) == {}
 
+def test_assert_has_calls_and_mock_calls():
+    obj = mock.MagicMock()
+    obj.one()
+    obj.two()
+    obj.three()
+    obj.four()
+
+    # https://docs.python.org/3/library/unittest.mock.html#unittest.mock.Mock.mock_calls
+    assert obj.mock_calls == [
+        mock.call.one(), mock.call.two(), mock.call.three(), mock.call.four(),
+    ]
+
+    # https://docs.python.org/3/library/unittest.mock.html#unittest.mock.Mock.assert_has_calls
+    obj.assert_has_calls([
+        mock.call.one(), mock.call.two(), mock.call.three(),
+    ])
+    with pytest.raises(AssertionError) as err:
+        obj.assert_has_calls([mock.call.one(), mock.call.three()])
+    assert str(err.value) == (
+        'Calls not found.\n'
+        'Expected: [call.one(), call.three()]\n'
+        'Actual: [call.one(), call.two(), call.three(), call.four()]'
+    )
 
 
 
